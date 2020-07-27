ace5f47bc92d7c12e24fcd91652d20994d09c0b5
coretta_java@yahoo.com <coretta@sj.com>
2020-07-27 09:34:26 -0500

    intial commit

---
+++ src/main/java/pages/FramesPage.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class FramesPage {
+    private WebDriver driver;
+
+    public FramesPage(WebDriver driver){
+        this.driver = driver;
+
+    }
+}
