590240d68ff172340a8e52e849365e3d8a9db71b
Coretta <coretta_java@yahoo.com>
2020-07-31 23:20:54 -0500

    added a method to take screenshots

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,13 +1,18 @@
 package base;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
 import utils.WindowManager;
 import utils.WindowsTabManager;
+
+import java.io.File;
 
 public class BaseTests {
 
@@ -35,6 +40,19 @@ public class BaseTests {
     @AfterClass
     public void tearDown(){
         driver.quit();
+    }
+
+    /*
+     * This will run after each test runs.
+     * The method will take a screenshot
+     * and save it as a file
+     */
+
+    @AfterMethod
+    public void takeScreenshot(){
+        var camera = (TakesScreenshot)driver;
+        File screenshot = camera.getScreenshotAs(OutputType.FILE);
+        System.out.println("Screenshot taken: " + screenshot.getAbsolutePath());
     }
 
     /*
