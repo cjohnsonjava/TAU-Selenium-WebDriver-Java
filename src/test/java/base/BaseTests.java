470915f8aad68a6177e3bf7261cb877362d1823f
Coretta <coretta_java@yahoo.com>
2020-08-02 19:19:51 -0500

    added EventFiringWebDriver

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -5,12 +5,14 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
+import utils.EventReporter;
 import utils.WindowManager;
 import utils.WindowsTabManager;
 
@@ -19,18 +21,18 @@ import java.io.IOException;
 
 public class BaseTests {
 
-    private WebDriver driver;//WebDriver object
+    private EventFiringWebDriver driver;//A specific instance of a WebDriver called the EventFiringWebDriver
     protected HomePage homePage;
 
     @BeforeClass
     public void setUp(){
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
-        driver = new ChromeDriver();
+        driver = new EventFiringWebDriver(new ChromeDriver());//an instance of the EventFiringWebDriver listens out for events
 //        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.register(new EventReporter());//listens out for events
         goHome();
 
 //        driver.manage().window().setSize(new Dimension(375,812));
-
 
     }
 
